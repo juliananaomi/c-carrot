0232c16d95788d47ac8ab77c98748628f2e9f04e
juliananaomi <jullyyamauti@gmail.com>
2020-10-09 22:05:01 -0300

    agenda

--- Lista 1/agenda.c
+++ Lista 1/agenda.c
@@ -2,25 +2,57 @@
 #include <stdlib.h>
 
 typedef struct {
-    char numero[11], nome[30];
-} Agenda;
+    char nome[30], numero[11];
+} Dados;
 
-void ler_dados(Agenda *p, int n_pessoas) {
-    int i;
-    for (i=0; i<n_pessoas; i++) {
-        printf("Pessoa %d - Entre com o nome: ", i+1);
-        scanf("%s", p[i].nome);
-        printf("Pessoa %d - Entre com o numero de telefone: ", i+1);
-        scanf("%s", p[i].numero);   
-    } 
+void ler_dados(Dados *p, int i) {
+    printf("Pessoa %d - Entre com o nome: ", i);
+    scanf("%s", p[i].nome);
+    printf("Pessoa %d - Entre com o numero de telefone: ", i);
+    scanf("%s", p[i].numero);  
+}
+
+void retirar(Dados *p, char *nome_usuario, int i) {
+    int j;
+    for (j=0; j <= i; j++) {
+        if (p[j].nome == nome_usuario) {
+            free(p[j].nome);
+            free(p[j].numero);
+        }
+    }
 }
 
 int main(void) {
-    int n_pessoas;
-    printf("Informe o numero de pessoas a serem armazenadas\n");
-    scanf("%d", &n_pessoas);
-    Agenda *pessoa;
-    pessoa = (Agenda *)malloc( n_pessoas*sizeof(Agenda));  
-    ler_dados(pessoa, n_pessoas); 
-    return(0); 
+    int n, i, j;
+    char usuario;
+    Dados *Registro;
+    printf("Deseja adicionar um número a sua Agenda?\n1-Sim \n2-Não\n");
+    scanf("%d", &n);
+    Registro = (Dados *)malloc(sizeof(Dados));
+    i=0; //Posição da pessoa no Registro
+    if(n==1){
+        ler_dados(Registro, i);
+        while(n==1) { // Sim
+            printf("Deseja adicionar/retirar mais um numero na Agenda?\n1-Adicionar \n2-Retirar \n3-Sair\n");
+            scanf("%d", &n);
+            if(n==1) {  //Adicionar
+                i+=1;
+                Registro = (Dados *)realloc(Registro, (i+1)*sizeof(Dados));
+                ler_dados(Registro, i);
+            }
+            else if (n==2) { //Retirar
+                printf("Qual o nome da pessoa?");
+                scanf("%s", &usuario);
+                retirar(Registro, &usuario, i);
+                n=1; //Não sair do while
+            }
+            else {  //Printar p/ o usuario qnd terminar
+                for (j=0; j <= i; j++) {
+                    printf("Nome: %s\n", Registro[j].nome);
+                    printf("Numero: %s\n", Registro[j].numero);
+                }
+            }      
+        }
+    }
+    else return(1); // Não
 }
